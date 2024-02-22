717ac4a4ed1a8561ad9548d0712fc9ffcdda72d2
Gomezco1105 <jgomez_65@cue.edu.co>
2024-02-21 20:09:23 -0500

    first commit

---
+++ email.js
@@ -0,0 +1,5 @@
+let email = "jgomez_65@cue.edu.co";
+let contraseña = "123456789";
+
+email == "jgomez_65@cue.edu.co" && contraseña == "123456789" ?
+console.log(`Señor usuario con correo ${email}, Bienvenido al sistema.`) : console.log(`El correo ${email} y/o la contraseña digitada son incorrectos.`);
